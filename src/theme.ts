e7247fac404664dd15900ab73646b6298482934b
onesol98 <71314689+hansolbangul@users.noreply.github.com>
2023-01-16 18:31:42 +0900

    feat: global theme 설정

---
+++ src/theme.ts
@@ -0,0 +1,21 @@
+import { Theme } from "@emotion/react";
+
+export const darkTheme: Theme = {
+    bgColor: '#2f3640',
+    textColor: 'white',
+    accentColor: '#9c88ff',
+    cardBgColor: 'transparent',
+};
+
+export const lightTheme: Theme = {
+    bgColor: 'whitesmoke',
+    textColor: 'black',
+    accentColor: '#9c88ff',
+    cardBgColor: 'white',
+};
+
+export const flex_center = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+}
