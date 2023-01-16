12aeaffed011cc2bb35fd050008d8a9512e5c611
onesol98 <71314689+hansolbangul@users.noreply.github.com>
2023-01-16 18:30:08 +0900

    feat: 글로벌 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import { ThemeProvider } from '@emotion/react';
+import React, { Suspense, useState } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { useRecoilState } from 'recoil';
+import { isDarkAtom } from './atoms';
+import { Footer } from './components/footer/Footer';
+import { GlobalStyle } from './global';
+import { Router } from './router/Router';
+import { darkTheme, lightTheme } from './theme';
 
 function App() {
+  const [isDark, setIsDark] = useRecoilState(isDarkAtom)
+
   return (
-    <div >
-    </div>
-  );
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <ReactQueryDevtools initialIsOpen={true} />
+      <GlobalStyle />
+      <Router />
+      <Footer />
+    </ThemeProvider>
+  )
 }
 
 export default App;
---
+++ src/global.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Global, css } from "@emotion/react";
+
+const style = (theme: any) => css`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        color: ${theme.textColor};
+        background-color: ${theme.bgColor};
+    }
+`;
+
+export const GlobalStyle = () => {
+    return <Global styles={style} />
+}
