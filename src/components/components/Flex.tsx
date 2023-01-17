fb2f4285b9e0ae01002b825bcd2e91433efa071e
onesol98 <71314689+hansolbangul@users.noreply.github.com>
2023-01-17 22:11:13 +0900

    feat: flex, direction 생성

---
+++ src/components/components/Flex.tsx
@@ -0,0 +1,55 @@
+/** @jsxImportSource @emotion/react */
+import React from "react";
+import { Props } from "../../services/interface";
+
+export const Direction = ({children, ...props}: Props) => {
+    return (
+        <div {...props} css={{
+            ...props.style,
+            display: 'flex',
+            flexDirection: 'column',
+        }}>
+            {children}
+        </div>
+    )
+}
+
+Direction.JustifyCenter = ({children, ...props}: Props) => {
+    return (
+        <div {...props} css={{
+            ...props.style,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+        }}>
+            {children}
+        </div>
+    )
+}
+
+Direction.AlignCenter = ({children, ...props}: Props) => {
+    return (
+        <div {...props} css={{
+            ...props.style,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+        }}>
+            {children}
+        </div>
+    )
+}
+
+Direction.Center = ({children, ...props}: Props) => {
+    return (
+        <div {...props} css={{
+            ...props.style,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            {children}
+        </div>
+    )
+}
