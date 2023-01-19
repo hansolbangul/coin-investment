de6d3ddbeb8b27e818fd87780ace6243cbbd3738
onesol98 <71314689+hansolbangul@users.noreply.github.com>
2023-01-19 18:12:12 +0900

    fix: 전역 스타일 경로 변경

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import styled from "@emotion/styled";
 import { MdLightMode, MdOutlineDarkMode } from "react-icons/md";
-import { flex_center } from "../theme";
+import { flex_center } from "../global";
 
 const spring = {
     type: "spring",
