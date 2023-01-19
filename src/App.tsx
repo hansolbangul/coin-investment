ff29e1e8a0908751f6e89e9445809f62e2246196
onesol98 <71314689+hansolbangul@users.noreply.github.com>
2023-01-19 18:09:57 +0900

    feat: 다크모드 리코일 밸류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import { ThemeProvider } from '@emotion/react';
 import React, { Suspense, useState } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { isDarkAtom } from './atoms';
 import { GlobalStyle } from './global';
 import { Router } from './router/Router';
 import { darkTheme, lightTheme } from './theme';
 
 function App() {
-  const [isDark, setIsDark] = useRecoilState(isDarkAtom)
+  // const [isDark, setIsDark] = useRecoilState(isDarkAtom)
+  const isDark = useRecoilValue(isDarkAtom);
 
   return (
     <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
