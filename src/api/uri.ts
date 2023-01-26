15ef11fc7174731b1b471620f1b09a25c01488c1
onesol98 <ruaenddl981028@naver.com>
2023-01-26 16:04:19 +0900

    feat: 소켓 baseUrl

--- src/api/uri.ts
+++ src/api/uri.ts
@@ -1,3 +1,5 @@
+export const UPBIT_SOCKET = 'wss://api.upbit.com/websocket/v1';
+
 export const UPBIT_API = 'https://api.upbit.com/v1';
 
 export const ICON_API = 'https://coinicons-api.vercel.app/api/icon';
